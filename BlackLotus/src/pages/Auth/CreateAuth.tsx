aa4a65e8ea3419360195ecf19717d06bd5556f37
Emily Salsman <emsalsman23@gmail.com>
2026-02-10 10:58:20 -0600

    added container to login page

--- BlackLotus/src/pages/Auth/CreateAuth.tsx
+++ BlackLotus/src/pages/Auth/CreateAuth.tsx
@@ -3,13 +3,18 @@ import Input from "../../components/shared/Input"
 
 const CreateAuth = () => {
     return (
-    <>
-        <Input />
-        <Input />
-        <Input />
-        <Button text="hello world" /> 
+    
+        <div className="container p-12 shadow-md rounded-sm">
+            <form>
+                <Input />
+                <Input />
+                <Input />
+                <Button text="hello world" /> 
+            </form>
+        </div>
+
         
-    </>
+    
     )
 
 

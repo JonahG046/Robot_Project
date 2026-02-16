81d0d8815d7d5c6c91c4f92933b7ecc9a83675ca
Emily Salsman <emsalsman23@gmail.com>
2026-02-16 10:18:23 -0600

    login page

--- BlackLotus/src/Routes.tsx
+++ BlackLotus/src/Routes.tsx
@@ -3,6 +3,7 @@ import CreateAuth from "./pages/Auth"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import CurrentJobs from "./pages/CurrentJobs"
+import Login from "./pages/Login"
 
 const Routes = () => {
     return (
@@ -12,6 +13,7 @@ const Routes = () => {
                 <Route path="/Home" component={Home}/>
                 <Route path="/About" component={About}/>
                 <Route path="/Current-jobs" component={CurrentJobs}/>
+                <Route path="/Login" component={Login}/>
             </Switch>
         </Router>
 

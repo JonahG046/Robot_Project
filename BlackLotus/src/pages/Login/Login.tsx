2a8e108f83819ff8a8ca8d8181474334b96e2f33
Emily Salsman <emsalsman23@gmail.com>
2026-02-23 14:50:00 -0600

    login

--- BlackLotus/src/pages/Login/Login.tsx
+++ BlackLotus/src/pages/Login/Login.tsx
@@ -1,9 +1,14 @@
 import Button from "../../components/shared/Button"
 import Input from "../../components/shared/Input"
+import RouteLink from "../../components/shared/RouteLink"
 
 const Login = () => {
     const submitHandler = (event: any) => {
         console.log("form submitted", event.target);
+    }
+
+    const inputHandler = (data:any) => {
+        return console.log("input", data)
     }
 
     return (
@@ -15,11 +20,11 @@ const Login = () => {
 
         <div className="p-12 shadow-md rounded-[4%] bg-violet-400">
             <form name="account"className="flex flex-col gap-4" onSubmit={submitHandler}>
-                <Input id="email-address" label="Email Address" placeholder="jane.doe@mnsu.edu" type="email" />
-                <Input id="password" label="Password" type="password" />
+                <Input id="email-address" label="Email Address" placeholder="jane.doe@mnsu.edu" type="email" callback={inputHandler} />
+                <Input id="password" label="Password" type="password" callback={inputHandler} />
                 <div className="w-1/2"/>
                 <div className="w-1/2 text-right">
-                    <Button className="bg-transparent" text="forgot password?" type="submit" />
+                    <RouteLink text="forgot password?" href="/home" />
                 </div>
                 <Button className="bg-violet-200 w-full" text="Log In" type="submit" />
             </form>

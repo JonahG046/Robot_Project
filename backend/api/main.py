92f9c954368047d6ed0d829ec9773d3756dd87da
Dylan Grant <dylan.grant@gmail.com>
2026-03-03 13:57:13 -0800

    cors was the problem

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# CORS configuration
+# In production, you should specify the allowed origins more securely
+origins = [
+    "http://localhost:5173"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # connect to database
 
@@ -11,13 +25,15 @@ app = FastAPI()
 async def root():
     content = {"message": "Hello World"}
     headers = {"X-Web-Framework": "FastAPI", "Content-Language": "en-US", "Content-Type": "application/json"}
-    return JSONResponse(content=content, headers=headers)
+    response = JSONResponse(content=content, headers=headers)
+
+    return response
 
 # @app.get("/profile")
 # async def root():
 
 #     #Pull name, email, location from database
-    
+
 
 #     return {"name": "John Smith",
 #             "email": "john.smith@mnsu.edu",

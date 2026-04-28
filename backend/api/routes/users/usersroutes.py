8080bcd74bcd99bb4a4f841777b6eca9c93403f6
Emily Salsman <emsalsman23@gmail.com>
2026-04-28 11:08:33 -0500

    user

--- backend/api/routes/users/usersroutes.py
+++ backend/api/routes/users/usersroutes.py
@@ -24,14 +24,17 @@ router = APIRouter()
 @router.get("/users/{user_id}", response_model=UserResponse)
 async def read_user(user_id: int, db: Session = Depends(get_db)):
     query = select(Users).where(Users.id == user_id)
-    result = db.execute(query).one()
-    user = UserResponse.model_validate(result).model_dump()
+    result = db.execute(query)
+
+    print(result)
+    # user = UserResponse.model_validate(result).model_dump()
+    user = result.scalars().one()
     
     print(user)
     if not result:
         return {"message": "User not found"}
 
-    return {"message": f"Hello World, User ID: {user.id}, Name: {user.name}"}
+    return user
 
 
 @router.get("/users", response_model=list[UserResponse])
@@ -39,6 +42,7 @@ async def get_users(db: Session = Depends(get_db)):
     print("Getting users from database...")
     query = select(Users)
     result = db.execute(query)
+    print(result)
     users = result.scalars().all()
     
     # scalers.all returns JSON
